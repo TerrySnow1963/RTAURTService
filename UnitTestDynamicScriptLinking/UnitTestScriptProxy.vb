0df4abbac6b8980c2316ca0b27f818f9adf9095f
Terry Snow <Terry.Snow@riotinto.com>
2020-06-22 11:10:12 +1000

    changed path name for test DLL

--- UnitTestDynamicScriptLinking/UnitTestScriptProxy.vb
+++ UnitTestDynamicScriptLinking/UnitTestScriptProxy.vb
@@ -5,8 +5,7 @@ Imports System.Reflection
 <TestClass()> Public Class UnitTestScriptProxy
 
     <TestMethod()> Public Sub TestConnectAssembyToSimpleScript()
-        Dim scriptAssembly As Assembly = Assembly.LoadFile("c:\Users\Terry\Documents\Visual Studio 2015\Projects\RTAURTService\URTVBQALSimpleScript\bin\Debug\URTVBQALSimpleScript.dll")
-
+        Dim scriptAssembly As Assembly = Assembly.LoadFile("c:\TGSProfile\source\repos\RTAURTService\URTVBQALSimpleScript\bin\Debug\URTVBQALSimpleScript.dll")
 
         Assert.IsNotNull(scriptAssembly)
 
