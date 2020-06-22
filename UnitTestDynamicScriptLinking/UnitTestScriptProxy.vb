08b71c0a3467b9c19ee381071dcb069bbe49ca0f
Terry Snow <Terry.Snow@riotinto.com>
2020-06-22 11:14:23 +1000

    changed scriptfolder file location

--- UnitTestDynamicScriptLinking/UnitTestScriptProxy.vb
+++ UnitTestDynamicScriptLinking/UnitTestScriptProxy.vb
@@ -5,7 +5,7 @@ Imports System.Reflection
 <TestClass()> Public Class UnitTestScriptProxy
 
     <TestMethod()> Public Sub TestConnectAssembyToSimpleScript()
-        Dim scriptAssembly As Assembly = Assembly.LoadFile("c:\TGSProfile\source\repos\RTAURTService\URTVBQALSimpleScript\bin\Debug\URTVBQALSimpleScript.dll")
+        Dim scriptAssembly As Assembly = Assembly.LoadFile("c:\TGSProfile\source\repos\terrysnow1963\RTAURTService\URTVBQALSimpleScript\bin\Debug\URTVBQALSimpleScript.dll")
 
         Assert.IsNotNull(scriptAssembly)
 
